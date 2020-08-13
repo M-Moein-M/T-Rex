38432060c09c3c0428dfb1534a0e9f7601f3b8a2
Moein <mohammadmoein.moradi@gamil.com>
2020-08-13 19:12:24 +0430

    Added movement of the obstacles in the game and they will self distruct when they get out of the game frame so we wond create infinite number of obstacles

--- app.js
+++ app.js
@@ -4,27 +4,57 @@ function gameInit() {
     const acceleration = -1.1;
 
 
-    const obstacleVelocity = 2;
+    const obstacleVelocity = -2.5;
 
     const minObstacleHeight = 30;
     const maxObstacleHeight = 80;
 
     let gameFrameDiv = document.getElementById('game-frame');
 
-    let obstacleInterval;
+    let createObstacleInterval;
+    let moveObstacleInterval; // this is used in moveAllObstacles function.
+    // This is Interval that moves all of the obstacles every couple of seconds
+
 
     function startGame() { // this function will start the game (starts new game to play again after losing)
         // and we will call this function at the very end of the 'gameInit' function
         const obstacleCreationTime = 2000; // 2000 ns, this indicates how fast an obstacle get shown on the game
-        obstacleInterval = setInterval(createObstacle, obstacleCreationTime);
+        createObstacleInterval = setInterval(createObstacle, obstacleCreationTime);
+
+        moveObstacleInterval = setInterval(moveAllObstacles, 10);
     }
 
+
+    function moveAllObstacles() {
+        let allObstacles = document.querySelectorAll('.obstacle');
+        for (let i = 0; i < allObstacles.length; i++) {
+            let id = allObstacles[i].getAttribute('id');
+            let obstacleElement = document.getElementById(id);
+            obstacleElement.style.left = (getElementPosition(obstacleElement, 'left') + 2 * obstacleVelocity).toString() + 'px';
+            // we use '2*obstacleVelocity' since we don't want to enlarge the obstacles too much but we want to obstacles to be faster
+            // we use obstacleVelocity to calculate maximum of obstacle width when we random its width
+
+            if (getElementPosition(obstacleElement, 'left') < -200) {// 200 is a reasonable amount for deleting the obstacle when it goes out of the game frame
+                gameFrameDiv.removeChild(obstacleElement);
+            }
+
+        }
+    }
+
+    let obstacleCount = 0; // This variable will keep track of the number of obstacles that were created so far.
+    // This will be used as ID of very obstacle
+    // ex. The first obstacle has the id of '0'
     function createObstacle() {
 
         let obstacle = document.createElement('div');
         obstacle.classList.add('game-object');
+        obstacle.classList.add('obstacle');
+
+        obstacle.id = obstacleCount.toString();
+        obstacleCount++;
+
         let obstacleHeight = Math.floor(Math.random() * 60) + 20; // generates random integer between 20 and 80
-        let obstacleWidth = Math.floor(Math.random() * (calcMaxObstacleWidth(obstacleHeight) - 40) + 20) // to make sure the obstacle has some width
+        let obstacleWidth = Math.floor(Math.random() * (calcMaxObstacleWidth(obstacleHeight) - 40) + 20); // to make sure the obstacle has some width
 
         let gameFrameDivWidth = gameFrameDiv.style.width = '1000px';
         console.log(gameFrameDivWidth);
@@ -33,17 +63,16 @@ function gameInit() {
         obstacle.style.width = obstacleWidth.toString() + 'px';
         obstacle.style.height = obstacleHeight.toString() + 'px';
 
-        let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        obstacle.style.backgroundColor =  randomColor;
 
+        let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        obstacle.style.backgroundColor = randomColor;
         gameFrameDiv.appendChild(obstacle);
     }
 
     function calcMaxObstacleWidth(obstacleHeight) { // will calculate how big we can make our obstacle
         // based on Physics calculation stored in assets directory
         return (2 / Math.abs(acceleration)) * Math.sqrt(-2 * acceleration * obstacleHeight +
-            initialVelocityY * initialVelocityY) * obstacleVelocity;
-
+            initialVelocityY * initialVelocityY) * Math.abs(obstacleVelocity);
     }
 
 
@@ -88,6 +117,7 @@ function gameInit() {
         if (event.code === 'ArrowUp')
             TRexJump();
     })
+
 
     startGame();
 }
